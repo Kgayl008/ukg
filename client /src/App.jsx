2d965cd6505a42e45f88fc36e953cd9c6d588e80
Kiyanna Gayle <Kgayl008@fiu>
2023-05-05 13:11:53 -0400

    add icon

--- client /src/App.jsx
+++ client /src/App.jsx
@@ -1,8 +1,16 @@
 import './App.scss';
 import Close from "../src/assets/icons/close.png"
+import { useState } from 'react';
 
 
 function App() {
+
+  const [buttonText, setButtonText] = useState('Send Message');
+  
+  const handleClick = () => {
+    setButtonText('Review');
+  };
+
     return (
         <>
           <div className='container'>
@@ -10,8 +18,8 @@ function App() {
             <p className="title">Tell us what’s happening?</p>
             <form action="">
                 <textarea type="text" className='input'></textarea>
-                <button className='button'>
-                    <span>Send message</span>
+                <button onClick={handleClick} className='button'>
+                    <span>{buttonText}</span>
                 </button>
             </form>
           </div>
