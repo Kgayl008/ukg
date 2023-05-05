23ed3b90e45fc16adc369174c8b1ad9bf65fcb7a
Kiyanna Gayle <Kgayl008@fiu>
2023-05-05 11:53:51 -0400

    finished style

--- client /src/App.jsx
+++ client /src/App.jsx
@@ -1,9 +1,12 @@
 import './App.scss';
+import Close from "../src/assets/icons/close.png"
+
 
 function App() {
     return (
         <>
           <div className='container'>
+          <img src={Close} alt=' close x icon' className='icon'/>
             <p className="title">Tell us what’s happening?</p>
             <form action="">
                 <textarea type="text" className='input'></textarea>
